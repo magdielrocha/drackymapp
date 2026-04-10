141a67690e009f314bd71b9e8605f17411683404
magdielrocha <magdiel002@gmail.com>
2026-04-09 22:37:41 -0300

    configuração inicial do projeto

---
+++ src/main/java/br/dev/mag/drackymapp/DrackymappApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mag.drackymapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrackymappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrackymappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/mag/drackymapp/DrackymappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mag.drackymapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrackymappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
