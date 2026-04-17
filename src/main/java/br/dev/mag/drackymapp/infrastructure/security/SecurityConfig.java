f2bf8600ec3958e2f11f30bed691470b0ce36e76
magdielrocha <magdiel002@gmail.com>
2026-04-17 15:00:08 -0300

    feat: cria o adaptador CustomUserDetails e CustomUserDetailsService

--- src/main/java/br/dev/mag/drackymapp/infrastructure/security/SecurityConfig.java
+++ src/main/java/br/dev/mag/drackymapp/infrastructure/security/SecurityConfig.java
@@ -1,7 +1,9 @@
-package br.dev.mag.drackymapp.config;
+package br.dev.mag.drackymapp.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,5 +33,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ src/main/java/br/dev/mag/drackymapp/services/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package br.dev.mag.drackymapp.services;
+
+import br.dev.mag.drackymapp.infrastructure.security.CustomUserDetails;
+import br.dev.mag.drackymapp.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+    }
+
+}
